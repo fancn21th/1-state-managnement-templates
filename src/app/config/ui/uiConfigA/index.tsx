07274ca671b21c75841871487feccbeabff3e16e
Fan, Yi-Jie <fancn21th@aliyun.com>
2025-10-22 14:17:43 +0800

    👌 IMPROVE: step2-3 extract component

--- src/app/config/ui/uiConfigA/index.tsx
+++ src/app/config/ui/uiConfigA/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import MultipleFields from "../../../../components/multipleFields";
 import { useConfigA } from "../../model/configA/hooks";
 
 export default function UiConfigA() {
@@ -8,11 +9,10 @@ export default function UiConfigA() {
   return (
     <div className="border p-4">
       Config A
-      <textarea
-        className="w-full h-64 p-4 font-mono text-sm border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none resize-vertical bg-gray-50"
+      <MultipleFields
         value={JSON.stringify(model, null, 2)}
-        onChange={(e) => updateModel(JSON.parse(e.target.value))}
-      ></textarea>
+        onChange={(newValue) => updateModel(JSON.parse(newValue))}
+      />
     </div>
   );
 }
---
+++ src/app/config/ui/uiConfigB/index.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function UiConfigA() {
+  return (
+    <div className="border p-4">
+      Config A
+      <textarea className="w-full h-64 p-4 font-mono text-sm border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none resize-vertical bg-gray-50"></textarea>
+    </div>
+  );
+}
